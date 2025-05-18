2d341524f6ea90d5d69947d36844b00f0c6d39e3
Cam3Loski11 <jfcamelo.2004@gmail.com>
2025-05-18 19:21:37 -0400

    Starting the carrusel, but there is a bunch of bugs to fix

---
+++ src/components/RomanticSwiper.jsx
@@ -0,0 +1,40 @@
+// src/components/RomanticSwiper.jsx
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, EffectFade } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+
+import "./RomanticSwiper.css";
+
+const images = [
+  "../../public/carrusel-imgs/foto1.jpeg",
+  "../../public/carrusel-imgs/foto2.jpeg",
+  "../../public/carrusel-imgs/foto3.jpeg",
+  "../../public/carrusel-imgs/foto4.jpeg",
+  "../../public/carrusel-imgs/foto5.jpeg",
+];
+
+export default function RomanticSwiper() {
+  return (
+    <Swiper
+      modules={[Autoplay, Pagination, EffectFade]}
+      effect="fade"
+      autoplay={{ delay: 5000, disableOnInteraction: false }}
+      pagination={{ clickable: true }}
+      loop={true}
+      slidesPerView={1}
+      spaceBetween={0}
+      className="my-romantic-swiper"
+    >
+      {images.map((src, index) => (
+        <SwiperSlide key={`slide-${index}`}>
+          <img src={src} alt={`Imagen ${index + 1}`} className="slide-image" />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
